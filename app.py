8d0ec14c57ccf0214ea2608989eb4f2b930b9b70
Chaitanya4000 <60195247+Chaitanya4000@users.noreply.github.com>
2020-12-16 16:34:54 +0530

    Add files via upload
    added / to file path of model

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 try:
-    modelpath = os.path.join(os.getcwd(),'Model\\PythonSalaryPredictionModel')
+    modelpath = os.path.join(os.getcwd(),'Model/PythonSalaryPredictionModel')
     with open(modelpath, 'rb') as f:
         model = pickle.load(f)
     print(modelpath,'This was success')
